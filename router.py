1dcec4790c6dae3886d13ad0bdaf1a6a9c1ad580
Julien Gaye <julien.gaye@gmail.com>
2018-08-20 22:25:01 +0200

    Upload function copied from Flask documentation
    Copied from http://flask.pocoo.org/docs/1.0/patterns/fileuploads/#uploading-files

--- router.py
+++ router.py
@@ -1,14 +1,47 @@
-from flask import (Flask, render_template, redirect, url_for)
+import os
+from flask import (Flask, flash, request, render_template, redirect, url_for)
+from werkzeug.utils import secure_filename
 
 # change type here
 # list(), download(), delete() and upload() reserved here
 from localFilesHelper import *
 
-app = Flask(__name__)
+UPLOAD_FOLDER = './testFolder'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'xlsx', 'pptx', 'docx'])
 
-@app.route("/")
-def hello():
-    return "This is the upload page"
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+  return '.' in filename and \
+    filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_document():
+  if request.method == 'POST':
+    # check if the post request has the file part
+    if 'file' not in request.files:
+      flash('No file part')
+      return redirect(request.url)
+    file = request.files['file']
+    # if user does not select file, browser also
+    # submit an empty part without filename
+    if file.filename == '':
+      flash('No selected file')
+      return redirect(request.url)
+    if file and allowed_file(file.filename):
+      filename = secure_filename(file.filename)
+      file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+      return redirect(url_for('index'))
+  return '''
+  <!doctype html>
+  <title>Upload new File</title>
+  <h1>Upload new File</h1>
+  <form method=post enctype=multipart/form-data>
+    <input type=file name=file>
+    <input type=submit value=Upload>
+  </form>
+  '''
 
 # Listing page
 @app.route("/index")
