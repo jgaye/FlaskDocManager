c6c83d6e43c50b258e3d24647dbb9872fa167fa8
Julien Gaye <julien.gaye@gmail.com>
2018-08-21 09:57:56 +0200

    Upload action performed from the helper

--- localFilesHelper.py
+++ localFilesHelper.py
@@ -1,11 +1,15 @@
+import os
 import pathlib
 import urllib.request
+from werkzeug.utils import secure_filename
 
-testDirectory = '/Users/Marvin/projects/FlaskDocManager/testFolder/'
+
+UPLOAD_FOLDER = '/Users/Marvin/projects/FlaskDocManager/testFolder/'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'xlsx', 'pptx', 'docx'])
 
 def list():
   documents = []
-  for currentFile in pathlib.Path(testDirectory).iterdir():  
+  for currentFile in pathlib.Path(UPLOAD_FOLDER).iterdir():  
     document = {}
     document['name'] = currentFile.name
     document['path'] = currentFile.as_uri()
@@ -14,7 +18,7 @@ def list():
 
 def download(document):
   # Download the file in this directory
-  fullPath = pathlib.Path(testDirectory + document)
+  fullPath = pathlib.Path(UPLOAD_FOLDER + document)
   urllib.request.urlretrieve(fullPath.as_uri(), document)
   # TODO ask the user where he wants to download the file
   # TODO check that the download was successful
@@ -24,9 +28,29 @@ def download(document):
 
 def delete(document):
 
-  fullPath = pathlib.Path(testDirectory + document)
+  fullPath = pathlib.Path(UPLOAD_FOLDER + document)
   fullPath.unlink()
 
   # TODO check that the deletion was successful
   # TODO give the user a log message to confirm the delete was successful
   return fullPath.as_uri() + " was successfully deleted"
+
+def allowed_file(filename):
+  return '.' in filename and \
+    filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def upload(request):
+  # check if the post request has the file part
+  if 'file' not in request.files:
+    return "No file part"
+  file = request.files['file']
+  # if user does not select file, browser also
+  # submit an empty part without filename
+  if file.filename == '':
+    return "No selected file"
+  if not allowed_file(file.filename):
+    return "File extension not allowed for upload"
+  if file and allowed_file(file.filename):
+    filename = secure_filename(file.filename)
+    file.save(os.path.join(UPLOAD_FOLDER, filename))
+    return filename + ' was successfully uploaded'
--- router.py
+++ router.py
@@ -1,42 +1,24 @@
-import os
 from flask import (Flask, flash, request, render_template, redirect, url_for)
-from werkzeug.utils import secure_filename
 
 # change type here
 # list(), download(), delete() and upload() reserved here
 from localFilesHelper import *
 
-UPLOAD_FOLDER = './testFolder'
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'xlsx', 'pptx', 'docx'])
-
 app = Flask(__name__)
 app.secret_key = b'mySecretKeyForFlash'
 
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-def allowed_file(filename):
-  return '.' in filename and \
-    filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 # Should I use the Flask-Uploads extension instead ?
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_document():
   if request.method == 'POST':
-    # check if the post request has the file part
-    if 'file' not in request.files:
-      flash('No file part')
-      return redirect(request.url)
-    file = request.files['file']
-    # if user does not select file, browser also
-    # submit an empty part without filename
-    if file.filename == '':
-      flash('No selected file')
-      return redirect(request.url)
-    if file and allowed_file(file.filename):
-      filename = secure_filename(file.filename)
-      file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-      flash(filename + ' was successfully uploaded')
-      return redirect(url_for('index'))
+    # Try to perform the upload and get a confirmation message
+    result = upload(request)
+
+    # Flash allows to show the confirmation message at index
+    flash(result)
+    return redirect(url_for('index'))
+
   return render_template('uploadPage.html')
 
 # Listing page
