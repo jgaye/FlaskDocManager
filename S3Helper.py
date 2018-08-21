141708ebc00e34b7bee01468a4621804824a57e9
Julien Gaye <julien.gaye@gmail.com>
2018-08-21 14:52:35 +0200

    Upload to S3 ok

--- S3Helper.py
+++ S3Helper.py
@@ -1,10 +1,13 @@
 import os
 import boto3, botocore
+from werkzeug.utils import secure_filename
 
 S3_BUCKET                 = os.environ.get("S3_BUCKET_NAME")
 S3_KEY                    = os.environ.get("S3_ACCESS_KEY")
 S3_SECRET                 = os.environ.get("S3_SECRET_ACCESS_KEY")
 S3_LOCATION               = 'http://{}.s3.amazonaws.com/'.format(S3_BUCKET)
+
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'xlsx', 'pptx', 'docx'])
 
 s3 = boto3.client(
    "s3",
@@ -31,5 +34,38 @@ def download(document):
 def delete(document):
   return "not implemented"
 
+def allowed_file(filename):
+  return '.' in filename and \
+    filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 def upload(request):
-  return "not implemented"
+  # check if the post request has the file part
+  if 'file' not in request.files:
+    return "No file part"
+  file = request.files['file']
+  # if user does not select file, browser also
+  # submit an empty part without filename
+  if file.filename == '':
+    return "No selected file"
+  if not allowed_file(file.filename):
+    return "File extension not allowed for upload"
+  if file and allowed_file(file.filename):
+    filename = secure_filename(file.filename)
+
+    # try to upload to S3
+    try:
+      s3.upload_fileobj(
+          file,
+          S3_BUCKET,
+          filename,
+          ExtraArgs={
+              "ContentType": file.content_type
+          }
+      )
+
+      return filename + ' was successfully uploaded'
+
+    except Exception as e:
+      # This is a catch all exception, edit this part to fit your needs.
+      print("Something Happened: ", e)
+      return str(e)
